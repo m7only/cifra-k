ee32a40cfc13cc3c411e61192a70bb04b856a78b
Anton Skryagin <ongranly@ngs.ru>
2024-03-05 21:18:23 +0700

    Init commit

---
+++ src/main/java/com/m7/cifrak/CifraKApplication.java
@@ -0,0 +1,13 @@
+package com.m7.cifrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifraKApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CifraKApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m7/cifrak/CifraKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m7.cifrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CifraKApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
